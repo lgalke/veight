1ebde8368838b1b3464fd02ff76617e291926c26
Lukas Galke <git@lpag.de>
2016-12-19 09:50:47 +0100

    reorganization to support Packs

--- SMmove.sh
+++ SMmove.sh
@@ -1,7 +1,7 @@
 VEIGHT=$( cd $(dirname "$0"); pwd -P)
-SOURCE_REPO=$VEIGHT/vim/$2
-DEST_REPO=$VEIGHT/vim/$3
 PACKAGE=$1
+SOURCE_REPO=$2
+DEST_REPO=$VEIGHT/$3
 echo "[V] Moving $PACKAGE from $SOURCE_REPO to $DEST_REPO"
 
 cd $SOURCE_REPO && git mv $PACKAGE $DEST_REPO
