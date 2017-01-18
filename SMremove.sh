8cf4742793f76ccd744adfb4e921c64af389f1ac
Lukas Galke <git@lpag.de>
2017-01-18 13:04:31 +0100

    jedi and more robustness

--- SMremove.sh
+++ SMremove.sh
@@ -3,8 +3,10 @@ if [ $# -lt 2 ]; then
 fi
 
 VEIGHT=$( cd $(dirname "$0"); pwd -P)
-REPO=$VEIGHT/vim/pack/$1
+REPO="$VEIGHT/vim/pack/$1"
+CACHE="$VEIGHT/.git/modules/vim/pack/$1"
 TARGET="$2"
 echo "[-] Removing $TARGET in $REPO"
-# cd $REPO && git rm -rf --ignore-unmatch $TARGET && rm -rf $TARGET
 cd $REPO && git rm $TARGET 
+echo "[-] Purging cached version of $TARGET in .git/modules"
+rm ".git/modules/vim/pack/$1"
